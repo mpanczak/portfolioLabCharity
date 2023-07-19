9f364dd57e827b91393d1c48aa9a70da6a7fac0e
mpanczak <panczakmateusz@gmail.com>
2023-07-19 18:02:30 +0200

    Institution

---
+++ src/main/java/pl/coderslab/charity/entity/Institution.java
@@ -0,0 +1,17 @@
+package pl.coderslab.charity.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "institutions")
+@Data
+public class Institution {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+}
---
+++ src/main/java/pl/coderslab/charity/service/InstitutionService.java
@@ -0,0 +1,9 @@
+package pl.coderslab.charity.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class InstitutionService {
+}
