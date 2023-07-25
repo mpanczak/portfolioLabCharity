397133662930877f2b001e0c99614c7602c56d81
mpanczak <panczakmateusz@gmail.com>
2023-07-25 20:18:13 +0200

    load data script update

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,6 +4,8 @@ INSERT INTO categories (id, name) VALUES (2, 'zabawki');
 
 INSERT INTO institutions (id, name, description) VALUES (1, 'Dbam o Zdrowie', 'Pomoc dzieciom z ubogich rodzin.');
 INSERT INTO institutions (id, name, description) VALUES (2, 'A kogo', 'Pomoc wybudzaniu dzieci ze śpiączki.');
+INSERT INTO institutions (id, name, description) VALUES (3, 'Dla dzieci', 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej.');
+INSERT INTO institutions (id, name, description) VALUES (4, 'Bez domu', 'Pomoc dla osób nie posiadających miejsca zamieszkania');
 
 
 INSERT INTO donations (id, city, pick_up_comment, pick_up_date, pick_up_time, quantity, street, zip_code) VALUES (1, 'Warszawa', 'Zdrówka', CURRENT_DATE, CURRENT_TIME, 2, 'Długa 13', '01-950');
